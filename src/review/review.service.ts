ffe14654f441dcaaccc05dfe445876f959c4d3fd
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-04-16 16:45:17 +0500

    Amazon v2.0

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { generateSlug } from 'src/utils/generate-slug';
+import { returnReviewObject } from './return-review.object';
+import { ReviewDto } from './review.dto';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll() {
+    return this.prisma.review.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      select: returnReviewObject,
+    });
+  }
+
+  async create(userId: number, dto: ReviewDto, productId: number) {
+    return this.prisma.review.create({
+      data: {
+        ...dto,
+        product: {
+          connect: {
+            id: productId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  async delete(id: number) {
+    return this.prisma.review.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
